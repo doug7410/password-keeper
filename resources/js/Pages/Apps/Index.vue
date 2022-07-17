ef6a926b21b6333ce0577731690829d715b26299
Doug Steinberg <d.steinberg79@gmail.com>
2022-07-17 15:39:59 -0400

    can filter passwords

--- resources/js/Pages/Apps/Index.vue
+++ resources/js/Pages/Apps/Index.vue
@@ -1,10 +1,22 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import BreezeInput from '@/Components/Input.vue';
+import {computed} from "vue"
+import { ref } from 'vue'
 
-defineProps({
+const props = defineProps({
     apps: Array,
 })
+
+const state = ref({
+    filter: ''
+})
+
+const name = ''
+const filtered = computed(() => props.apps.filter(app => {
+  return  app.name.toLowerCase().includes(state.value.filter.toLowerCase())
+}))
 </script>
 
 <template>
@@ -19,7 +31,14 @@ defineProps({
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div v-for="app in apps" class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="mb-2">Filter:
+                    <BreezeInput id="name"
+                                 type="text"
+                                 class="mt-1 block w-full"
+                                 v-model="state.filter"
+                    />
+                </div>
+                <div v-for="app in filtered" class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <p class="text-blue-900 font-bold pb-2">{{ app.name }}</p>
                         <a class="text-blue-700 block mb-2 hover:underline" :href="app.url">{{ app.url }}</a>
